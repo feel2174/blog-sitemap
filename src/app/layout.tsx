8f714d4a71881e2e5cb8fc17c2a884a6daa27276
feel2174 <feel2174@gmail.com>
2026-03-13 18:03:11 +0900

    feat: add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import Script from "next/script";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -93,6 +94,7 @@ export default function RootLayout({
           strategy="afterInteractive"
         />
         {children}
+        <Analytics />
       </body>
     </html>
   );
